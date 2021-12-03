d23cbf3a0c3016f89e67c05d89bde84ecd90807c
yo76317 <yo76317@gmail.com>
2021-12-03 16:36:11 +0800

    學校sql檔

---
+++ sql/yo76317.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-12-03 09:34:58
+-- 伺服器版本： 10.4.21-MariaDB
+-- PHP 版本： 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `yo76317`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ad`
+--
+
+CREATE TABLE `ad` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `sh` tinyint(1) UNSIGNED NOT NULL,
+  `intro` varchar(64) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `ad`
+--
+
+INSERT INTO `ad` (`id`, `name`, `sh`, `intro`) VALUES
+(1, '245038959_5148008455216238_5907972707057045573_n.jpg', 0, 'mumu');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `opt` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `topic_id` int(11) UNSIGNED NOT NULL,
+  `count` int(11) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `options`
+--
+
+INSERT INTO `options` (`id`, `opt`, `topic_id`, `count`) VALUES
+(1, '選項一:', 1, 15),
+(2, '選項二:', 1, 2),
+(3, '選項三:', 1, 1),
+(4, '選項四:', 1, 2),
+(5, '2', 2, 1),
+(6, '2', 2, 0),
+(7, '2', 2, 0),
+(8, '2', 2, 0),
+(9, '好', 3, 0),
+(10, '不好', 3, 0),
+(11, '1', 4, 1),
+(12, '2', 4, 0),
+(13, '3', 4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `topics`
+--
+
+CREATE TABLE `topics` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `topic` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 傾印資料表的資料 `topics`
+--
+
+INSERT INTO `topics` (`id`, `topic`) VALUES
+(1, '1'),
+(2, '2'),
+(3, '今晚吃火鍋?'),
+(4, '123');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `accout` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `password` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `email` varchar(24) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `gender` varchar(1) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `birthday` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `ad`
+--
+ALTER TABLE `ad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
+--
+ALTER TABLE `ad`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
